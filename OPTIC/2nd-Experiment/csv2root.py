1e21bacc6d87eba32b4c7def15953873fad6ccdc
ibeuler <ibohamed27@gmail.com>
2025-03-24 16:17:50 +0300

    data update

--- OPTIC/2nd-Experiment/csv2root.py
+++ OPTIC/2nd-Experiment/csv2root.py
@@ -2,6 +2,20 @@ import pandas as pd
 import uproot
 import numpy as np
 
+"""
+USAGE: 
+... LAB-Reports/OPTIC/2nd-Experiment$ python3
+
+Python 3.10.12 (main, Feb  4 2025, 14:57:36) [GCC 11.4.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+
+>>> import csv2root
+>>> csv2root.csv2ROOT(FileRelativePath="DATA/Table_Data.csv")
+ROOT file saved as DATA/Table_Data.root
+>>> quit()
+
+"""
+
 def csv2ROOT(FileRelativePath="data.csv"):
 	# Step 1: Read CSV file
 	csv_file = FileRelativePath  # Replace with your CSV file path
